e21efc8613936d650edf1d4807f3f394796bc73d
Sven van den Tweel <sven.vd.tweel@gmail.com>
2020-08-11 08:38:33 +0200

    added try and catch around nullpointer code. updated StartupReceiver to avoid exception, increased versionnumber

--- app/src/main/java/nl/invissvenska/bdobosstimers/list/BossAdapter.java
+++ app/src/main/java/nl/invissvenska/bdobosstimers/list/BossAdapter.java
@@ -39,46 +39,50 @@ public class BossAdapter extends RecyclerView.Adapter<BossViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull BossViewHolder holder, int position) {
-        if (position == 0) {
-            holder.boss1.setImageAlpha(70);
-            holder.boss2.setImageAlpha(70);
-            holder.timeLeft.setText(holder.boss1.getContext().getString(R.string.spawned));
-            holder.name.setAlpha(0.4f);
-            holder.spawnTime.setAlpha(0.4f);
-            holder.timeLeft.setAlpha(0.4f);
-        } else {
-            holder.boss1.setImageAlpha(255);
-            holder.boss2.setImageAlpha(255);
-            holder.name.setAlpha(1f);
-            holder.spawnTime.setAlpha(1f);
-            holder.timeLeft.setAlpha(1f);
-        }
-        final Boss boss = bosses.get(position);
-        holder.name.setText(boss.getName().replace("&", " & "));
-        holder.spawnTime.setText(boss.getTimeSpawn());
-        holder.boss1.setImageResource(boss.getBossOneImageResource());
-        if (boss.getBossTwoImageResource() != null) {
-            holder.boss2.setImageResource(boss.getBossTwoImageResource());
-            holder.boss2.setVisibility(View.VISIBLE);
-        } else {
-            holder.boss2.setVisibility(View.GONE);
-        }
-        if (holder.timer != null) {
-            holder.timer.cancel();
-        }
-        if (boss.getMinutesToSpawn() > 0 && position != 0) {
-            holder.timer = new CountDownTimer(boss.getMinutesToSpawn() * 60 * 1000, 1000L) {
-                @Override
-                public void onTick(long millisUntilFinished) {
-                    holder.timeLeft.setText(TimeHelper.getInstance().secondsToHoursAndMinutesAndSeconds(millisUntilFinished / 1000));
-                }
+        try {
+            if (position == 0) {
+                holder.boss1.setImageAlpha(70);
+                holder.boss2.setImageAlpha(70);
+                holder.timeLeft.setText(holder.boss1.getContext().getString(R.string.spawned));
+                holder.name.setAlpha(0.4f);
+                holder.spawnTime.setAlpha(0.4f);
+                holder.timeLeft.setAlpha(0.4f);
+            } else {
+                holder.boss1.setImageAlpha(255);
+                holder.boss2.setImageAlpha(255);
+                holder.name.setAlpha(1f);
+                holder.spawnTime.setAlpha(1f);
+                holder.timeLeft.setAlpha(1f);
+            }
+            final Boss boss = bosses.get(position);
+            holder.name.setText(boss.getName().replace("&", " & "));
+            holder.spawnTime.setText(boss.getTimeSpawn());
+            holder.boss1.setImageResource(boss.getBossOneImageResource());
+            if (boss.getBossTwoImageResource() != null) {
+                holder.boss2.setImageResource(boss.getBossTwoImageResource());
+                holder.boss2.setVisibility(View.VISIBLE);
+            } else {
+                holder.boss2.setVisibility(View.GONE);
+            }
+            if (holder.timer != null) {
+                holder.timer.cancel();
+            }
+            if (boss.getMinutesToSpawn() > 0 && position != 0) {
+                holder.timer = new CountDownTimer(boss.getMinutesToSpawn() * 60 * 1000, 1000L) {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        holder.timeLeft.setText(TimeHelper.getInstance().secondsToHoursAndMinutesAndSeconds(millisUntilFinished / 1000));
+                    }
 
-                @Override
-                public void onFinish() {
-                    holder.timeLeft.setText(holder.boss1.getContext().getString(R.string.spawned));
-                }
-            };
-            holder.timer.start();
+                    @Override
+                    public void onFinish() {
+                        holder.timeLeft.setText(holder.boss1.getContext().getString(R.string.spawned));
+                    }
+                };
+                holder.timer.start();
+            }
+        } catch (NullPointerException e) {
+            Log.e("BDO", "something was null: " + e.getMessage());
         }
     }
 
