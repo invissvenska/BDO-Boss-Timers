990d45f25056017b0bfee8570bf2535e101718ae
Sven van den Tweel <sven.vd.tweel@gmail.com>
2021-09-11 17:45:36 +0200

    Minimize inapp review code

--- app/src/main/java/nl/invissvenska/bdobosstimers/MainActivity.java
+++ app/src/main/java/nl/invissvenska/bdobosstimers/MainActivity.java
@@ -2,17 +2,12 @@ package nl.invissvenska.bdobosstimers;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.google.android.play.core.review.ReviewInfo;
 import com.google.android.play.core.review.ReviewManager;
 import com.google.android.play.core.review.ReviewManagerFactory;
-import com.google.android.play.core.review.model.ReviewErrorCode;
-import com.google.android.play.core.tasks.OnCompleteListener;
-import com.google.android.play.core.tasks.Task;
 import com.jakewharton.threetenabp.AndroidThreeTen;
 
 import nl.invissvenska.bdobosstimers.logging.ReleaseTree;
@@ -21,9 +16,6 @@ import timber.log.Timber;
 import static nl.invissvenska.bdobosstimers.Constants.ACTION.NOTIFICATION_CLICKED;
 
 public class MainActivity extends AppCompatActivity {
-
-    ReviewManager reviewManager;
-    ReviewInfo reviewInfo = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
             Timber.plant(new ReleaseTree());
         }
 
-        getReviewInfo();
         onNewIntent(getIntent());
 
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
@@ -49,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
-        if (intent.getBooleanExtra(NOTIFICATION_CLICKED, false)){
+        if (intent.getBooleanExtra(NOTIFICATION_CLICKED, false)) {
             startReviewFlow();
         }
     }
@@ -62,27 +53,18 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void getReviewInfo() {
-        reviewManager = ReviewManagerFactory.create(getApplicationContext());
-        Task<ReviewInfo> manager = reviewManager.requestReviewFlow();
-        manager.addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                reviewInfo = task.getResult();
-            } else {
-                Timber.e("In App ReviewFlow failed to start");
-                Toast.makeText(getApplicationContext(), "In App ReviewFlow failed to start", Toast.LENGTH_LONG).show();
-            }
-        });
-    }
-
-    public void startReviewFlow() {
-        if (reviewInfo != null) {
-            Task<Void> flow = reviewManager.launchReviewFlow(this, reviewInfo);
-            flow.addOnCompleteListener(task -> Toast.makeText(getApplicationContext(), "In App Rating complete", Toast.LENGTH_LONG).show());
-        }
-        else {
-            Timber.e("In App Rating failed");
-            Toast.makeText(getApplicationContext(), "In App Rating failed", Toast.LENGTH_LONG).show();
-        }
+    private void startReviewFlow() {
+        ReviewManager reviewManager = ReviewManagerFactory.create(getApplicationContext());
+        reviewManager.requestReviewFlow()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        reviewManager.launchReviewFlow(this, task.getResult())
+                                .addOnCompleteListener(flow -> {
+                                    // The flow has finished. The API does not indicate whether the user
+                                    // reviewed or not, or even whether the review dialog was shown. Thus, no
+                                    // matter the result, we continue our app flow.
+                                });
+                    }
+                });
     }
 }
